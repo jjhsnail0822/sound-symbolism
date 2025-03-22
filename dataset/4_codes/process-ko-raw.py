fcad5f0b76967c579884b968ff0e2b06dcea54e0
jjhsnail0822 <jjhsnail0822@gmail.com>
2025-03-22 20:36:53 +0900

    Fix merge issues

--- dataset/4_codes/process-ko-raw.py
+++ dataset/4_codes/process-ko-raw.py
@@ -4,12 +4,9 @@ import os
 import re
 
 DIR_PATH = 'dataset/0_raw/nat/standard_kor_dic_20250306'
-<<<<<<< HEAD
 INVALID_WORD_LIST = ['-이', '-히']
 VALID_DEFINITION_LIST = [' 소리.', ' 모양.', ' 상태.', ' 느낌.'] # onomatopoeia: 소리, ideophone: 모양, 상태, 느낌 according to the Korean dictionary publishing standard
 
-=======
->>>>>>> 127e5c6 (Add korean data and codes)
 files_dir = os.listdir(DIR_PATH)
 # remove hidden files
 files_dir = [f for f in files_dir if not f.startswith('.')]
@@ -29,26 +26,16 @@ def extract_definition_list(d):
         for comm_pattern_info in pos_info['comm_pattern_info']:
             for sense_info in comm_pattern_info['sense_info']:
                 definition = sense_info['definition']
-                definition = definition.split(' ‘')[0].strip() # remove sentences like '‘가득01「1」’보다 센 느낌을 준다.'
-                definitions.append(definition)
+                if '. ‘' in definition:
+                    definition = definition.split(' ‘')[0] # remove sentences like '‘가득01「1」’보다 센 느낌을 준다.'
+                definitions.append(definition.strip())
     return definitions
 
-<<<<<<< HEAD
 def is_onomatopoeia(original_word, definition_list):   
     if any(invalid in original_word for invalid in INVALID_WORD_LIST):
         return False
     for definition in definition_list:
         if any(valid in definition for valid in VALID_DEFINITION_LIST):
-=======
-def is_onomatopoeia(original_word, definition_list):
-    invalid_word_list = ['-이', '-히']
-    valid_def_list = [' 소리.', ' 모양.', ' 상태.', ' 느낌.'] # onomatopoeia: 소리, ideophone: 모양, 상태, 느낌 according to the Korean dictionary publishing standard
-    
-    if any(invalid in original_word for invalid in invalid_word_list):
-        return False
-    for definition in definition_list:
-        if any(valid in definition for valid in valid_def_list):
->>>>>>> 127e5c6 (Add korean data and codes)
             return True
     return False
 
@@ -64,36 +51,25 @@ for d in ds:
             else:
                 onomatopoeias.append({'word': word, 'definitions': definitions}) # add new word
 
-<<<<<<< HEAD
 def extract_unique_meaning(definitions): # representative meaning should not be like 'OO의 준말.'
     for definition in definitions:
         if any(valid in definition for valid in VALID_DEFINITION_LIST):
             return definition
     return definitions[0] if definitions else ''
 
-=======
->>>>>>> 127e5c6 (Add korean data and codes)
 # save to csv
 with open('dataset/0_raw/nat/ko.csv', 'w', encoding='utf-8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['word', 'meaning', 'ref'])
     for o in onomatopoeias:
-<<<<<<< HEAD
         writer.writerow([o['word'], extract_unique_meaning(o['definitions']), '표준국어대사전 20250306'])
-=======
-        writer.writerow([o['word'], o['definitions'][0], '표준국어대사전 20250306'])
->>>>>>> 127e5c6 (Add korean data and codes)
 
 for o in onomatopoeias:
     o['url'] = 'https://stdict.korean.go.kr'
     o['found'] = True
-<<<<<<< HEAD
     o['meaning'] = extract_unique_meaning(o['definitions'])
-=======
-    o['meaning'] = o['definitions'][0]
->>>>>>> 127e5c6 (Add korean data and codes)
     o['ref'] = '표준국어대사전 20250306'
 
 # save to json
-with open('dataset/1_preprocess/nat/ko_past.json', 'w', encoding='utf-8') as f:
+with open('dataset/1_preprocess/nat/ko.json', 'w', encoding='utf-8') as f:
     json.dump(onomatopoeias, f, ensure_ascii=False, indent=4)
