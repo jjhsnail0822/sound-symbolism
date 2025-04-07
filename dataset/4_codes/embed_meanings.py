618a999fceec6938404f3ae6ebacc8a835b73b3b
jjhsnail0822 <jjhsnail0822@gmail.com>
2025-04-07 20:42:26 +0900

    Add embedding code

---
+++ dataset/4_codes/embed_meanings.py
@@ -0,0 +1,57 @@
+import argparse
+from dotenv import load_dotenv
+import json
+from openai import OpenAI
+import os
+from pathlib import Path
+import pickle
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description='Get embeddings for meanings')
+parser.add_argument('-l', '--language', type=str, default='ko', help='Language code (default: ko)', choices=['ko', 'en', 'ja', 'fr'])
+args = parser.parse_args()
+
+LANGUAGE = args.language
+
+# Load environment variables from .env.local file
+env_path = Path('.env.local')
+load_dotenv(dotenv_path=env_path)
+
+# Get API key from environment variables
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def get_embedding(text):
+    """
+    Get the embedding for a given text using OpenAI's API.
+    """
+    response = client.embeddings.create(
+        input=text,
+        model="text-embedding-3-large"
+    )
+    return response.data[0].embedding
+
+with open(f'dataset/1_preprocess/nat/{LANGUAGE}.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+embeddings = []
+for word in tqdm(data):
+    if not word['found']:
+        continue
+    # is meaning a list?
+    if isinstance(word['meaning'], list):
+        meaning = word['meaning'][0]
+    else:
+        meaning = word['meaning']
+    # get embedding
+    embedding = get_embedding(meaning)
+    embeddings.append({
+        'word': word['word'],
+        'meaning': meaning,
+        'embedding': embedding
+    })
+
+# Save the embeddings to a file
+with open(f'dataset/1_preprocess/nat/{LANGUAGE}_embeddings.pkl', 'wb') as f:
+    pickle.dump(embeddings, f)
