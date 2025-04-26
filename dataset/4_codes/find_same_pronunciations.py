0ea781ec7a482da490df58160823541dea86141d
jjhsnail0822 <jjhsnail0822@gmail.com>
2025-04-26 11:10:21 +0900

    Change filepaths

--- dataset/4_codes/find_same_pronunciations.py
+++ dataset/4_codes/find_same_pronunciations.py
@@ -2,10 +2,7 @@ import json
 import itertools
 import argparse
 
-LANG = 'ko'
-# LANG = 'fr'
-
-def find_same_pronunciations(input_path):
+def find_same_pronunciations(input_path, lang):
     with open(input_path, 'r', encoding='utf-8') as f:
         records = json.load(f)
     ipa_map = {}
@@ -15,7 +12,7 @@ def find_same_pronunciations(input_path):
         if ipa and word:
             ipa_map.setdefault(ipa, []).append(word)
     # for each IPA with more than one word, print all unique pairs
-    with open(f'dataset/1_preprocess/nat/same_pronunciations_{LANG}.txt', 'w', encoding='utf-8') as f:
+    with open(f'dataset/1_preprocess/nat/same_pronunciations_{lang}.txt', 'w', encoding='utf-8') as f:
         for ipa, words in ipa_map.items():
             if len(words) > 1:
                 for w1, w2 in itertools.combinations(words, 2):
@@ -23,9 +20,7 @@ def find_same_pronunciations(input_path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lang", type=str, default='ko', help='Language to process')
-    parser.add_argument("--input", type=str, help="Input file path")
+    parser.add_argument("--lang", "-l", type=str, default='en', help='Language to process')
     args = parser.parse_args()
-    ipa_file = f'dataset/1_preprocess/nat/{args.lang}_ipa.json'
-    # ipa_file = f'/scratch2/sheepswool/workspace/sound-symbolism/dataset/1_preprocess/nat/{args.lang}_ipa_filtered.json'
-    find_same_pronunciations(ipa_file)
+    ipa_file = f'dataset/1_preprocess/nat/{args.lang}.json'
+    find_same_pronunciations(ipa_file, args.lang)
