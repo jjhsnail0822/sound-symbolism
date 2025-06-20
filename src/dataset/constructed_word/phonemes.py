bb55317117f2a1791e158b47b97a211cd27200bf
linea77 <sunahan@korea.ac.kr>
2025-06-20 16:44:14 +0900

    update: make ipa map language-independent #51

--- src/dataset/constructed_word/phonemes.py
+++ src/dataset/constructed_word/phonemes.py
@@ -1,44 +1,18 @@
-
+# Higher-order factors of sound symbolism
+# Excluded ipa symbols are commented out
 IPA_MAP = {
-    "en":{
         "consonants":{
-            "stops":  ["p", "b", "t", "d", "k", "g"],
-            "nasals": ["m", "n", "ŋ"],
-            "fricatives":  ["f", "v", "ð", "θ", "s", "z", "ʃ", "ʒ", "h" ],
-            "affricates" : ["t͡ʃ", "d͡ʒ"],
-            "approximants" : ["l", "ɹ", "j", "w"],
-            "all":["p", "b", "t", "d", "k", "g", 
-                   "m", "n", "ŋ", 
-                   "f", "v", "ð", "θ", "s", "z", "ʃ", "ʒ", "h",
-                   "t͡ʃ", "d͡ʒ",
-                   "l", "ɹ", "j", "w"]
+            "sonorants": ["l", "m", "n"], 
+            "voiced_ficatives":["v","ð",  "z",], 
+            "voiceless_fricatives": ["f","s","ʃ",],
+            "voiced_stops": [ "b", "d",  "g" ], 
+            "voiceless_stops": ["p", "t", "k" ],
+            "excluded": ["ŋ", "w",  "j", "ɹ","ʒ",  "θ", "h", "t͡ʃ", "d͡ʒ"]
         },
         "vowels": {
-            "monothongs":  ["i", "ɪ","ʊ", "u", "ɛ", "ə", "ɔ","æ", "ʌ", "ɑ" ],
-            "dipthongs":  ["aj","aw", "oj", "ej", "ow" ],
-            "all": ["i", "ɪ","ʊ", "u", "ɛ", "ə", "ɔ","æ", "ʌ", "ɑ",
-                    "aj","aw", "oj", "ej", "ow"]
-
-        }
-    },
-    "ko": {
-        "consonants": {
-            "stops": ["p","p͈","pʰ", "t", "t͈", "tʰ", "k", "k͈", "kʰ" ],
-            "nasals": ["m", "n", "ŋ"],
-            "fricatives":["s", "s͈","h"],
-            "affricates":["t͡ɕ", "t͈͡ɕ", "t͡ɕʰ"],
-            "approximants" : ["l"],
-            "all" : ["p","p͈","pʰ", "t", "t͈", "tʰ", "k", "k͈", "kʰ",
-                     "m", "n", "ŋ",
-                     "s", "s͈","h",
-                     "t͡ɕ", "t͈͡ɕ", "t͡ɕʰ"]
-        },
-        "vowels" : {
-            "monothongs" : [ "i", "ɛ", "u","o", "ɯ","ʌ",  "a"],
-            "dipthongs" : ["wi", "we", "wʌ", "ja", "jʌ", "jo", "ju","jɛ", "ɰi"],
-            "all" : ["i", "ɛ", "u","o", "ɯ","ʌ",  "a",
-                     "wi", "we", "wʌ", "ja", "jʌ", "jo", "ju","jɛ", "ɰi"
-            ]
+            "front": ["i", "ej"], 
+            "back": [ "ɑ", "ow" ], 
+            "excluded": ["ɪ", "ɛ", "æ","ə", "ʊ", "u","ʌ",  "ɔ", "aw","aj", "oj"]
         }
     }
-}
+
