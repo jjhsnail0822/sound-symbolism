2390f11c4f7b2302e4e95944cca194485886384b
RomanticBox <sheepswool@yonsei.ac.kr>
2025-07-04 10:24:57 +0900

    Update semantic dimension heatmap visualization code

--- src/analysis/heatmap/compute_attention_score.py
+++ src/analysis/heatmap/compute_attention_score.py
@@ -11,6 +11,7 @@ from semdim_heatmap import QwenOmniSemanticDimensionVisualizer as qwensemdim
 
 ipa_to_feature_map = json.load(open("./data/constructed_words/ipa_to_feature.json"))
 feature_to_score_map = json.load(open("./data/constructed_words/feature_to_score.json"))
+data_path = "data/processed/nat/semantic_dimension/semantic_dimension_binary_gt.json"
 '''
 1. Input : combination of data type, language, attention type, computation type, layers and heads
 data_type : audio, word, romanization, ipa
@@ -32,6 +33,7 @@ class AttentionScoreCalculator(qwensemdim):
     def __init__(
         self,
         model_path: str,
+        data_path: str,
         tokenizer_path: str,
         data_type: str,
         lang: str,
@@ -40,21 +42,30 @@ class AttentionScoreCalculator(qwensemdim):
         layer: int,
         compute_type: str,
     ):
-        super().__init__()
+        super().__init__(
+            model_path=model_path,
+            data_path=data_path,
+            output_dir="results/experiments/understanding/attention_heatmap",
+            exp_type="semantic_dimension",
+            data_type=data_type,
+            max_tokens=32,
+            temperature=0.0
+        )
         self.model_path = model_path
+        self.data_path = data_path
         self.tokenizer_path = tokenizer_path
         self.data_type = data_type
         self.lang = lang
         self.layer_type = layer_type
         self.compute_type = compute_type
-        self.semantic_dimension_map = {
-            
-        }
+        self.semantic_dimension_map = [
+            "good", "bad", "beautiful", "ugly", "pleasant", "unpleasant", "strong", "weak", "big", "small", "rugged", "delicate", "active", "passive", "fast", "slow", "sharp", "round", "realistic", "fantastical", "structured", "disorganized", "orginary", "unique", "interesting", "uninteresting", "simple", "complex", "abrupt", "continuous", "exciting", "calming", "hard", "soft", "happy", "sad", "harsh", "mellow", "heavy", "light", "inhibited", "free", "masculine", "feminine", "solid", "nonsolid", "tense", "relaxed", "dangerous", "safe"
+        ]
     
-    def load_matrix(self, layer_type: str, data_type: str, attention_type: str, word_tokens: str, dimension1: str, dimension2: str, lang: str):
-        return super().read_matrix(layer_type, data_type, attention_type, word_tokens, dimension1, dimension2, lang)
+    def load_matrix(self, layer_type:str, data_type:str, attention_type:str, word_tokens:str, dimension1:str, dimension2:str, lang:str):
+        return super().read_matrix(layer_type, attention_type, word_tokens, dimension1, dimension2, lang)
     
-    def convert_token_to_phoneme(self, tokens: list[str], alpha_to_ipa_map:dict):
+    def convert_token_to_phoneme(self, tokens:list[str], alpha_to_ipa_map:dict):
         phoneme_list = []
         for token in tokens:
             if token in alpha_to_ipa_map:
@@ -63,75 +74,27 @@ class AttentionScoreCalculator(qwensemdim):
                 phoneme_list.append(token)
         return phoneme_list
     
-    def compute_layer_wise_matrix():
+    def compute_layer_wise_matrix(self, data_type:str, lang:str):
+        """
+        
+        """
         pass
     
     def compute_head_wise_matrix():
         pass
     
     def compute_phoneme_semdim_relation():
-        semantic_dimension_map = {
-            "good": "",
-            "bad": "",
-            "beautiful": "",
-            "ugly": "",
-            "pleasant": "",
-            "unpleasant": "",
-            "strong": "",
-            "weak": "",
-            "big": "",
-            "small": "",
-            "rugged": "",
-            "delicate": "",
-            "active": "",
-            "passive": "",
-            "fast": "",
-            "slow": "",
-            "sharp": "",
-            "round": "",
-            "realistic": "",
-            "fantastical": "",
-            "structured": "",
-            "disorganized": "",
-            "orginary": "",
-            "unique": "",
-            "interesting": "",
-            "uninteresting": "",
-            "simple": "",
-            "complex": "",
-            "abrupt": "",
-            "continuous": "",
-            "exciting": "",
-            "calming": "",
-            "hard": "",
-            "soft": "",
-            "happy": "",
-            "sad": "",
-            "harsh": "",
-            "mellow": "",
-            "heavy": "",
-            "light": "",
-            "inhibited": "",
-            "free": "",
-            "masculine": "",
-            "feminine": "",
-            "solid": "",
-            "nonsolid": "",
-            "tense": "",
-            "relaxed": "",
-            "dangerous": "",
-            "safe": ""
-        }
+
         pass
     
     def run(self, dimension1, dimension2, computation_type, heads, layers, langs):
-        
-        self.load_matrix()
         self.convert_token_to_phoneme()
-        if self.compute_type == "flow":
-            self.compute_layer_wise_matrix()
-        elif self.compute_type == "heatmap":
-            self.compute_head_wise_matrix()
+        for dimension1, dimension2 in self.semantic_dimension_map:
+            self.load_matrix()
+            if self.compute_type == "flow":
+                self.compute_layer_wise_matrix()
+            elif self.compute_type == "heatmap":
+                self.compute_head_wise_matrix()
         
     def matrix_layer_level_computation(self, filtered_attention_matrix, computation_type, head:Union[int, str], layer:Union[int, str], phoneme_mean_map:dict):
         # Convert to tensor if it's not already
@@ -189,3 +152,20 @@ class AttentionScoreCalculator(qwensemdim):
                 # For 1D or other dimensions, just use as is
                 computed_matrix = filtered_attention_matrix
         return computed_matrix
+
+if __name__ == "__main__":
+    asc = AttentionScoreCalculator(
+        model_path="Qwen/Qwen2.5-Omni-7B",
+        data_path=data_path,
+        tokenizer_path="Qwen/Qwen2.5-Omni-7B",
+        data_type="original",
+        lang="en",
+        layer_type="generation",
+        head="all",
+        layer="all",
+        compute_type="heatmap"
+    )
+    
+    attention_matrix, dimension1, dimension2, answer, word_tokens, option_tokens =asc.load_matrix(layer_type="generation", data_type="original", attention_type="generation_attention", word_tokens="zoon", dimension1="big", dimension2="small", lang="en")
+    breakpoint()
+    
