e5786d5ffcde2d22dceddce74e868d43c5470e03
linea77 <sunahan@korea.ac.kr>
2025-07-08 23:31:18 +0900

    feat: add pronounciation dictionary of constructed words and test other languages (#61)

--- src/analysis/aligning/mfa.py
+++ src/analysis/aligning/mfa.py
@@ -1,4 +1,4 @@
-import csv
+import json
 import time
 import subprocess
 import unicodedata
@@ -8,41 +8,37 @@ class MFAWrapRunner():
     '''
     CLI 도구인 MFA를 Python에서 Wrapping하여 명령어 실행 
     '''
-    def __init__(self):
+    def __init__(self, lang, dict_path=None ):
+        self.model_name = self.set_model_name(lang)
+        self.dict_path = dict_path
+        
         self.check_mfa_installation()
-        self.model_name = None
-
         if not self.check_mfa_installation():
             raise ModuleNotFoundError("MFA is not installed. Run `conda install -c conda-forge montreal-forced-aligner`")      
-
-    def run(self, lang, corpus_dir, output_dir, dict_path=None, validate=False, pretrained=True, clean=True):
-        self.set_model_name(lang)
-
-        if not self.check_model_downloaded():
-            download = True
-        else:
-            download = False
-
-        if download:
-            self.download_model('acoustic')
         
-            if pretrained:
+        # self.validate_dictionary(dict_path)
+
+        if not self.check_model_downloaded('acoustic'):
+            self.download_model('acoustic') 
+
+        if not self.check_model_downloaded('dictionary'):
+            if dict_path is None:
                 self.download_model('dictionary')
+          
 
 
+    def run(self, corpus_dir, output_dir, validate=False, clean=True):
         if validate:
             print("[INFO] Validate Corpus")
             self.validate_corpus(corpus_dir)
             print("[INFO] Finished")
 
-            self.validate_dictionary(dict_path)
-
         print("[INFO] Start Aligning")
         tic = time.time()
-        if pretrained:
+        if self.dict_path is None:
             self.align_with_pretrained_model(corpus_dir, output_dir, clean)
         else:
-            self.align_with_custom_dict(corpus_dir, dict_path, output_dir, clean)
+            self.align_with_custom_dict(corpus_dir, self.dict_path, output_dir, clean)
         toc = time.time()
         processed_time = toc - tic
         print(f"[INFO] Finished: {processed_time}s ")
@@ -51,9 +47,8 @@ class MFAWrapRunner():
     def check_mfa_installation(self):
         try:
             result = subprocess.run(['mfa', 'version'], capture_output=True, text=True)
-            print("[DEBUG] process running result: ", result)
             if result.returncode == 0:
-                print(f'MFA is installed: {result.stdout.strip()}')
+                print(f'MFA is already installed: {result.stdout.strip()}')
                 return True
             else:
                 print("MFA is not installed")
@@ -62,25 +57,21 @@ class MFAWrapRunner():
             return False
 
     def set_model_name(self, lang):
+        if lang not in ['art', 'en', 'ko', 'ja', 'fr']:
+            print(f'[ERROR] Language {lang} is not supported')
+            raise ValueError
+        
         model_map = {
             'en': 'english_us_arpa',
             'fr': 'french_mfa',
             'ja': 'japanese_mfa',
             'ko': 'korean_mfa'
         }
-        if lang == 'art':
-            lang = 'en'
-        if lang not in model_map:
-            raise ValueError
-        
-        self.model_name = model_map[lang]
+        return model_map['en'] if lang == 'art' else model_map[lang]
 
-    def check_model_downloaded(self, lang=None):
-        if lang is not None:
-            self.set_model_name(lang)
-
+    def check_model_downloaded(self, model_type ):
         try:
-            cmd = ['mfa', 'model', 'download', 'acoustic', self.model_name]
+            cmd = ['mfa', 'model', 'download', model_type, self.model_name]
             result = self.run_mfa_command(cmd)
 
             if result.returncode == 0:
@@ -94,14 +85,10 @@ class MFAWrapRunner():
             raise e
 
  
-    def download_model(self, model_type):
-        if self.model_name is None:
-            print('run `set_model_name(lang)` method first')
-
+    def download_model(self, model_type:str):
         cmd = ['mfa', 'model', 'download', model_type, self.model_name]   
         self.run_mfa_command(cmd)
 
-    
     def validate_corpus(self, corpus_dir):
         cmd = ['mfa', 'validate', corpus_dir, self.model_name, self.model_name ]
         self.run_mfa_command(cmd)
@@ -110,7 +97,7 @@ class MFAWrapRunner():
         cmd = ['mfa', 'validate_dictionary', dict_path]
         self.run_mfa_command(cmd)
 
-    def align_with_custom_dict(self, corpus_dir, dict_path, output_dir,  clean):
+    def align_with_custom_dict(self, corpus_dir, dict_path, output_dir, clean):
         cmd = ['mfa', 'align', corpus_dir, dict_path, self.model_name, output_dir ]
         if clean:
             cmd.append('--clean')
@@ -126,7 +113,7 @@ class MFAWrapRunner():
             print('call `set_model_name(lang)` method first.')
             return 
         
-        cmd = ['mfa', 'align', corpus_dir, self.model_name, self.model_name, output_dir]
+        cmd = ['mfa', 'align', corpus_dir, self.model_name, self.model_name, str(output_dir)]
          
         if clean:
             cmd.append('--clean')
@@ -135,6 +122,7 @@ class MFAWrapRunner():
     
     def run_mfa_command(self, cmd):
         try:
+            cmd = list(map(str, cmd))
             print(f'[INFO] Running... `{" ".join(cmd)}`')
             result = subprocess.run(cmd, capture_output=True, text=True, check=True)
             print('[INFO] Finished')
@@ -144,63 +132,92 @@ class MFAWrapRunner():
         except subprocess.CalledProcessError as e:
             print(f'[Error] {e.stderr}')
             return -1
+        except Exception as e:
+            print('ERROR', cmd)
    
     def list_models(self, model_type):
         cmd = ['mfa', 'model', 'download', model_type, '--list']
         return self.run_mfa_command(cmd)
 
 
-class MFADataPrep:
-    def __init__(self, corpus_dir):
-        self.corpus_dir = Path(corpus_dir)
-    
-    def prepare_data(self):
-        audio_files = self.corpus_dir.glob("*.wav")
-        for audio_file in audio_files:
-            transcription = unicodedata.normalize('NFKC', audio_file.stem)
-            lab_filepath = self.corpus_dir / f'{transcription}.lab'
-            self._create_lab_file(transcription, lab_filepath)
-        
-
-    def _create_lab_file(self, trascription, output_path):
+def create_lab_files(corpus_dir):
+    audio_files = Path(corpus_dir).glob("*.wav")
+    for audio_file in audio_files:
+        transcription = unicodedata.normalize('NFKC', audio_file.stem)
+        output_path = Path(corpus_dir) / f'{transcription}.lab'
         with open(output_path, 'w', encoding='utf-8') as f:
-            f.write(trascription)
+            f.write(transcription)
         print(f"[INFO] LAB file saved: {output_path}")
 
-    def create_custom_dict(self, word_ipa_pairs, dict_path):
-       
-        with open(dict_path, 'w', encoding='utf-8') as f:
-            for word, ipa in word_ipa_pairs:
-                if word != 'alphabet':
-                    ipa = ipa.upper()
-                    f.write(f"{word}\t{' '.join(ipa)}\n")
-        print(f"[INFO] Pronounciation dictionary saved: {dict_path}")
 
-        
+
+def create_custom_dict(words_path, dict_path):
+    ipa_to_arpa = {
+    "l": "L",
+    "m": "M",
+    "n": "N",
+    "v": "V",
+    "ð": "DH",
+    "z": "Z",
+    "f": "F",
+    "s": "S",
+    "ʃ": "SH",
+    "b": "B",
+    "d": "D",
+    "ɡ": "G",
+    "p": "P",
+    "t": "T",
+    "k": "K",
+    "i": "IY0",
+    "ej": "EY0",
+    "ɑ": "AA0",
+    "ow": "OW0"
+    }    
+   
+    with open(words_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    word_ipa_pairs = [( item['word'], item['ipa']) for item in data['art']]
+    
+    for word, ipa in word_ipa_pairs:
+        arpa = [ipa_to_arpa[symbol] for symbol in ipa.split(' ')]
+        with open(dict_path,'a', encoding='utf-8' ) as f:
+            f.write(f"{word}\t{' '.join(arpa)}\n")
+    print(f"[INFO] Pronounciation dictionary saved: {dict_path}")
+
+            
 
 
 def main():
-    corpus_dir = './example'
-    output_dir = './output'
-    pretrained = True
+
+    lang = 'ko'
+    root = Path('/home/sunahan/workspace/sound-symbolism')
+    data_dir  = root / 'data' / 'processed'
+    if lang == 'art':
+        dict_path = data_dir / 'art' / 'resources' / 'ARPA_pronunciation_dict.txt'
+        words_path = data_dir / 'art' / 'constructed_words.json' 
+        corpus_dir = data_dir / 'art' / 'tts'
+        output_dir = data_dir / 'art' / 'textgrids' 
+    else:
+        dict_path = None
+        words_path = data_dir / 'nat' / f'{lang}.json'
+        corpus_dir = data_dir / 'nat' / 'tts' / lang
+        output_dir = data_dir / 'nat' / 'textgrids' / lang
+
+
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    prep = MFADataPrep(corpus_dir=corpus_dir)
-    prep.prepare_data()
+    # 1. Create Corpus - (.wav, .lab) pairs
+    create_lab_files(corpus_dir=corpus_dir)
 
-    # Artificial(Constructed) Words (Nonwords)#####
-    dict_path = './nonwords_pronunciation_dict.txt'
-
-    # words_path = '/home/sunahan/workspace/sound-symbolism/data/processed/art/outputs/constructed_nonwords.csv'
-    # with open(words_path, 'r', encoding='utf-8') as f:
-    #     reader = csv.reader(f)
-    #     word_ipa_pairs = [ (word, ipa) for _, ipa, word in reader]
-    #    prep.create_custom_dict(word_ipa_pairs,dict_path)
-    pretrained = False
-    ################################################
+    # 2. Create Dictionary
+    if lang == 'art':
+        create_custom_dict(words_path, dict_path)
     
-    mfa = MFAWrapRunner()
-    mfa.run(lang='en', corpus_dir=corpus_dir, output_dir=output_dir, dict_path=str(dict_path), pretrained=pretrained)
+    mfa = MFAWrapRunner(lang, dict_path=dict_path)
+    mfa.run(
+            corpus_dir=corpus_dir, 
+            output_dir=output_dir
+            )
 
 
 
--- src/dataset/constructed_word/run.sh
+++ src/dataset/constructed_word/run.sh
@@ -1,9 +1,11 @@
+#############
 ROOT="/home/sunahan/workspace/sound-symbolism"
+##############
+
 IPA_DICT_DIR="${ROOT}/data/raw/ipa_dict"
-
 DATA_DIR="${ROOT}/data/processed/art"
-
 RESOURCE_DIR="${DATA_DIR}/resources"
+WORDS_PATH="${DATA_DIR}/constructed_words.json"
 SEMANTIC_DIM_PATH="${DATA_DIR}/semantic_dimension/semantic_dimension_binary_gt.json"
 
 cd "${ROOT}/src/dataset/constructed_word"
@@ -15,12 +17,13 @@ python3 process_ipa_word_dict.py \
 
 python3 generate_word.py \
     --data_dir=$RESOURCE_DIR \
-    --output_path=$SEMANTIC_DIM_PATH \
+    --output_path=$WORDS_PATH \
     --ensure_nonword
 
 python3 annotate_score.py \
     --data_dir=$RESOURCE_DIR \
-    --json_path=$SEMANTIC_DIM_PATH
+    --json_path=$WORDS_PATH \
+    --output_dir=$SEMANTIC_DIM_PATH
 
 python3 ../audio/tts_generation_google.py \
     --word_type='art' \
