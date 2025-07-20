4bc204b92448ae2c80e7317e23c82a4d18918ac7
RomanticBox <sheepswool@yonsei.ac.kr>
2025-07-20 18:24:00 +0900

    Update attention score computation code

--- src/analysis/heatmap/run.sh
+++ src/analysis/heatmap/run.sh
@@ -3,4 +3,4 @@
 # sbatch --gres=gpu:1 -p dell_rtx3090 -q big_qos /scratch2/sheepswool/workspace/sound-symbolism/src/analysis/heatmap/run.sh --pty bash -i
 # python src/analysis/heatmap/semdim_heatmap.py --max-samples 5000 --data-type audio --flip
 # python src/analysis/heatmap/semdim_heatmap.py --max-samples 3000 --data-type ipa --constructed
-python src/analysis/heatmap/batch_semdim_heatmap.py --max-samples 5000 --data-type ipa --constructed --flip
+python src/analysis/heatmap/batch_semdim_heatmap.py --max-samples 5000 --data-type audio --constructed
