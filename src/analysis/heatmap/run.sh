4d67907603b42cc0635964cd3b27c08ef9856d3c
RomanticBox <sheepswool@yonsei.ac.kr>
2025-11-13 22:36:07 +0900

    Remove non-submission content on main

--- src/analysis/heatmap/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# python src/analysis/heatmap/semdim_heatmap.py --max-samples 5000 --data-type audio --flip
-# python src/analysis/heatmap/semdim_heatmap.py --max-samples 3000 --data-type ipa --constructed
-# python src/analysis/heatmap/batch_semdim_heatmap.py --max-samples 2000 --data-type audio --languages ko
-
-python src/analysis/heatmap/multi_threading_attention_computation.py
-# python src/analysis/heatmap/compute_attention_by_language.py
-# python src/analysis/heatmap/batch_semdim_heatmap.py --max-samples 3000 --data-type audio --constructed --flip
